b29e379c923c20428efba8ec676c90b576692190
atis <atis.elsts@gmail.com>
2020-04-21 13:51:05 +0300

    TSCH: fix channel_offset in the logs if taken from a packet, and refactor TSCH channel / channel_offset selection inside a slot

--- os/net/mac/tsch/tsch-slot-operation.h
+++ os/net/mac/tsch/tsch-slot-operation.h
@@ -67,11 +67,10 @@ extern int tsch_current_burst_count;
  * The offset to the ASN and performs a hopping sequence lookup.
  *
  * \param asn A given ASN
- * \param channel_offset Link's channel offset
- * \param p Packet that can override the link's channel offset
+ * \param channel_offset Given channel offset
  * \return The resulting channel
  */
-uint8_t tsch_calculate_channel(struct tsch_asn_t *asn, uint16_t channel_offset, struct tsch_packet *p);
+uint8_t tsch_calculate_channel(struct tsch_asn_t *asn, uint16_t channel_offset);
 /**
  * Checks if the TSCH lock is set. Accesses to global structures outside of
  * interrupts must be done through the lock, unless the sturcutre has
