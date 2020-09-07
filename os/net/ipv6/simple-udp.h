9d67a765dfbbd2c5e7e7f80cdb5c41498ccd9fe9
alexrayne <alexraynepe196@gmail.com>
2020-09-07 20:56:04 +0300

    *<NG:ipv6/simple-udp.h - provided proxy to ip/simple-udp.h, for NG code compat
    --HG--
    branch : ng-work

---
+++ os/net/ipv6/simple-udp.h
@@ -0,0 +1,17 @@
+/*
+ * uiplib.h
+ *
+ *  Created on: 02/09/2020
+ *      Author: alexrayne <alexraynepe196@gmail.com>
+ *-------------------------------------------------------------
+ *      wraper of simple-udp.h for compatibility with NextGen
+ */
+
+#ifndef OS_NET_IPV6_SIMPLEUDP_H_
+#define OS_NET_IPV6_SIMPLEUDP_H_
+
+
+#include "net/ip/simple-udp.h"
+
+
+#endif /* OS_NET_IPV6_SIMPLEUDP_H_ */
