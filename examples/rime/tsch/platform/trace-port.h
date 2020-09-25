6084606648fd79882a1be6f792e3955da17beb28
alexrayne <alexraynepe196@gmail.com>
2020-09-25 13:17:43 +0300

    !examples:rime tsch - provide default platform headers
    --HG--
    branch : ng-work

---
+++ examples/rime/tsch/platform/trace-port.h
@@ -0,0 +1,10 @@
+/*
+ * trace-port.h
+ *
+ *  Created on: 25/09/2020
+ *      Author: netuser
+ *      dummy trace-port for any platform
+ */
+
+#ifndef EXAMPLES_6TISCH_SIMPLE_NODE_TRACE_PORT_H_
+#endif /* EXAMPLES_6TISCH_SIMPLE_NODE_TRACE_PORT_H_ */
