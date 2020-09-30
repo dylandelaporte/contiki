457c99c4af41d5f107fdba310bcc4126c4876b25
alexrayne <alexraynepe196@gmail.com>
2020-09-30 16:00:33 +0300

    !border-router-cooja.sh now validly detects contiki path by defaults
    --HG--
    branch : ng-work

--- tests/17-tun-rpl-br/01-border-router-cooja.sh
+++ tests/17-tun-rpl-br/01-border-router-cooja.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # Contiki directory
+
+if  [ -z "$1" ]
+then 
+CONTIKI=$(dirname "$0")/../..
+else
 CONTIKI=$1
+fi
 
 # Simulation file
 BASENAME=$(basename $0 .sh)
+
+echo basename:$BASENAME
 
 bash test-border-router.sh $CONTIKI $BASENAME fd00::204:4:4:4 60
